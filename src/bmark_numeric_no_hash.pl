56b405cc5f4af5f4ea126a74396ebb6027216108
Ankit Pati <contact@ankitpati.in>
2021-02-28 03:00:27 +0530

    Perl numeric with no hash conversion

---
+++ src/bmark_numeric_no_hash.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Benchmark qw(:hireswallclock timeit);
+
+sub benchmark {
+    return map { $_ => $_ * $_ } 0..1000;
+}
+
+# Total, wallclock time for lots of runs.
+print ((timeit 10000, \&benchmark)->real, "\n");
+
+# Verify correctness, and compare with other benchmarked code.
+my %ret = benchmark;
+print "$_,$ret{$_}\n" foreach sort { $a <=> $b } keys %ret;
