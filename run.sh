77df9d36eba7ce870275103c0abfd7cb8835be5c
Ankit Pati <contact@ankitpati.in>
2021-04-19 16:16:16 +0530

    run.sh: check pypy3 perf

--- run.sh
+++ run.sh
@@ -8,5 +8,11 @@ test "$#" -ne 0 && echo "Usage: $me" && exit 1
 ITERATIONS=10000
 BMARK_OPTS=("$mydir/data/"{raw,{,numeric_}processed}_data.csv "$ITERATIONS")
 
+echo 'CPython'
 "$mydir/src/bmark.py" "${BMARK_OPTS[@]}"
+echo
+echo 'PyPy'
+pypy3 "$mydir/src/bmark.py" "${BMARK_OPTS[@]}"
+echo
+echo 'Perl'
 "$mydir/src/bmark.pl" "${BMARK_OPTS[@]}"
